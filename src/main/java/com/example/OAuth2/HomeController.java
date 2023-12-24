3d2d0402e3a3e70bb399786991d39014372c8acf
V3NOM1977 <rishabhrawat1977@gmail.com>
2023-12-25 02:00:31 +0530

    first commit

---
+++ src/main/java/com/example/OAuth2/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.OAuth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "Hello Wrld...!!";
+    }
+
+}
---
+++ src/main/java/com/example/OAuth2/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.OAuth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(withDefaults())
+                .formLogin(withDefaults())
+                .build();
+    }
+
+}
